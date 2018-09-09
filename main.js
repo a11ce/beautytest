f3c517b063e67eee2c1c73936329734a6e766c5d
a11ce <a11ce@protonmail.com>
2018-09-08 23:19:26 -0400

    Calculation/loading page

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ function upload() {
 
 function redirect() {
 if (document.getElementById('fileInput').value != "") {
-  window.location.href = "results.html";
+  window.location.href = "calculate.html";
 }
+}
+
+function showScore() {
+  setTimeout(goToScore, 5000);
+}
+
+function goToScore() {
+  window.location.href = "results.html";
 }
